4d31e21e528c8f7af182c67cf60f2a14abdf9dab
Mencho Menchev <m.menchev@seeburger.com>
2026-04-09 14:33:31 +0300

    Optimise import registrations and removals

---
+++ topology-manager/src/main/java/org/apache/aries/rsa/topologymanager/importer/EndpointDescriptionFilter.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.rsa.topologymanager.importer;
+
+import java.util.Objects;
+
+import org.osgi.service.remoteserviceadmin.EndpointDescription;
+
+class EndpointDescriptionFilter {
+	private final String filter;
+	private final EndpointDescription endpointDescription;
+
+	EndpointDescriptionFilter(String filter, EndpointDescription endpointDescription) {
+		this.filter = filter;
+		this.endpointDescription = endpointDescription;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		EndpointDescriptionFilter other = (EndpointDescriptionFilter) obj;
+		return filter.equals(other.filter) && endpointDescription.equals(other.endpointDescription);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(filter, endpointDescription);
+	}
+
+	String getFilter() {
+		return filter;
+	}
+
+	EndpointDescription getEndpoint() {
+		return endpointDescription;
+	}
+}
